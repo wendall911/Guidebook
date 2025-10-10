dd71b2b41b50f44be694cf6b1efdac732908c2a3
Wendall Cada <wendallc@83864.com>
2025-10-10 11:59:06 -0700

    Remove hand edited resources.

--- Fabric/src/main/java/guidebook/data/test/GuidebookTestLanguageProvider.java
+++ Fabric/src/main/java/guidebook/data/test/GuidebookTestLanguageProvider.java
@@ -7,6 +7,8 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 
 import net.minecraft.core.HolderLookup;
 
+import guidebook.api.GuidebookAPI;
+
 public class GuidebookTestLanguageProvider extends FabricLanguageProvider {
 
     public GuidebookTestLanguageProvider(FabricDataOutput dataOutput, CompletableFuture<HolderLookup.Provider> registryLookup) {
@@ -15,23 +17,27 @@ public class GuidebookTestLanguageProvider extends FabricLanguageProvider {
 
     @Override
     public void generateTranslations(HolderLookup.Provider provider, TranslationBuilder builder) {
-        addItem(builder, "guide_book", "Guide Book");
-        addItem(builder, "guide_book.undefined", "Invalid book: no ID defined");
-        addItem(builder, "guide_book.invalid", "Invalid book: %s");
-        addItem(builder, "comprehensive_test_book.name", "Comprehensive Test Book");
-        addItem(builder, "comprehensive_test_book.landing", "Test book with all the features.");
-        addItem(builder, "test_book_1.name", "Test Book 1");
-        addItem(builder, "test_book_1.landing", "Not as fun landing text but still here.");
-        addItem(builder, "test_book_2.name", "Test Book 2");
-        addItem(builder, "test_book_2.landing", "Landing text! It even supports $(2)colors$() and $(4)the $(bold)like$()!");
-        addItem(builder, "test_completion.name", "Test Advancement-Driven Book");
-        addItem(builder, "test_completion.landing", "This book changes colors when you unlock entries!");
-        addItem(builder, "pamphlet.name", "Test Pamphlet");
-        addItem(builder, "pamphlet.landing", "A test pamphlet, which is a book with only one category.");
+        addGuidebookItem(builder, "guide_book", "Guide Book");
+        addGuidebookItem(builder, "guide_book.undefined", "Invalid book: no ID defined");
+        addGuidebookItem(builder, "guide_book.invalid", "Invalid book: %s");
+        addGuidebookItem(builder, "comprehensive_test_book.name", "Comprehensive Test Book");
+        addGuidebookItem(builder, "comprehensive_test_book.landing", "Test book with all the features.");
+        addGuidebookItem(builder, "test_book_1.name", "Test Book 1");
+        addGuidebookItem(builder, "test_book_1.landing", "Not as fun landing text but still here.");
+        addGuidebookItem(builder, "test_book_2.name", "Test Book 2");
+        addGuidebookItem(builder, "test_book_2.landing", "Landing text! It even supports $(2)colors$() and $(4)the $(bold)like$()!");
+        addGuidebookItem(builder, "test_completion.name", "Test Advancement-Driven Book");
+        addGuidebookItem(builder, "test_completion.landing", "This book changes colors when you unlock entries!");
+        addGuidebookItem(builder, "pamphlet.name", "Test Pamphlet");
+        addGuidebookItem(builder, "pamphlet.landing", "A test pamphlet, which is a book with only one category.");
     }
 
-    private void addItem(TranslationBuilder builder, String id, String text) {
-        builder.add("item.guidebook:" + id, text);
+    private void addGuidebookItem(TranslationBuilder builder, String id, String text) {
+        add(builder, "item." + GuidebookAPI.MODID + ":" + id, text);
+    }
+
+    private void add(TranslationBuilder builder, String id, String text) {
+        builder.add(id, text);
     }
 
 }
