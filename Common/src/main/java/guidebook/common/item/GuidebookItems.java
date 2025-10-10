44d4a5cade16995cf5622cc937c76ffe60592727
Wendall Cada <wendallc@83864.com>
2025-10-10 11:56:37 -0700

    Generate item textures.

--- Common/src/main/java/guidebook/common/item/GuidebookItems.java
+++ Common/src/main/java/guidebook/common/item/GuidebookItems.java
@@ -10,6 +10,13 @@ import guidebook.api.GuidebookAPI;
 public class GuidebookItems {
 
     public static final ResourceLocation BOOK_ID = GuidebookAPI.prefix("guide_book");
+    public static final ResourceLocation BOOK_BLUE_ID = GuidebookAPI.prefix("book_blue");
+    public static final ResourceLocation BOOK_BROWN_ID = GuidebookAPI.prefix("book_brown");
+    public static final ResourceLocation BOOK_CYAN_ID = GuidebookAPI.prefix("book_cyan");
+    public static final ResourceLocation BOOK_GRAY_ID = GuidebookAPI.prefix("book_gray");
+    public static final ResourceLocation BOOK_GREEN_ID = GuidebookAPI.prefix("book_green");
+    public static final ResourceLocation BOOK_PURPLE_ID = GuidebookAPI.prefix("book_purple");
+    public static final ResourceLocation BOOK_RED_ID = GuidebookAPI.prefix("book_red");
     public static final Item BOOK = new ItemModBook();
 
     public static void submitItemRegistrations(BiConsumer<ResourceLocation, Item> consumer) {
--- Fabric/src/main/java/guidebook/data/FabricDatagenInitializer.java
+++ Fabric/src/main/java/guidebook/data/FabricDatagenInitializer.java
@@ -27,7 +27,7 @@ public class FabricDatagenInitializer implements DataGeneratorEntrypoint {
     public static void configureCommonDatagen(FabricDataGenerator.Pack pack) {
         FabricTagProvider.BlockTagProvider fabricBlockTagProvider = pack.addProvider(FabricBlockTagProvider::new);
         pack.addProvider((dataOutput, registryFuture) -> new GuidebookItemTagsProvider(dataOutput, registryFuture, fabricBlockTagProvider.contentsGetter()));
-        //pack.addProvider(GuidebookItemModelProvider::new);
+        pack.addProvider(GuidebookItemModelProvider::new);
         //pack.addProvider(GuidebookRecipeProvider::new);
         //pack.addProvider(GuidebookAdvancementProvider::new);
         pack.addProvider(GuidebookLanguageProvider::new);
